bc04434cb0c8c549a1f19e3ab0363e4f37f379c0
ckwogus37 <ckwogus37@gmail.com>
2024-07-05 16:44:12 +0900

    Spring boot 기본 폴더 구조 생성

---
+++ src/main/java/com/example/CleanUp_Spring_Boot/entity/user.java
@@ -0,0 +1,9 @@
+package com.example.CleanUp_Spring_Boot.entity;
+
+public class user {
+    private Integer id;
+    private String email;
+    private String password;
+    private String nickname;
+    private Integer total_score;
+}
